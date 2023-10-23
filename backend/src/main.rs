f5e0ae4562c3aca187a9e6fb157663497f14269a
philippderroole <philippderroole@gmail.com>
2023-10-23 21:51:55 +0200

    remove .next

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,5 +1,7 @@
 use std::error::Error;
 
+use sqlx::Row;
+
 async fn create_expense(expense: &Expense, pool: &sqlx::PgPool) -> Result<(), Box<dyn Error>> {
     let query = "INSERT INTO expenses (name, amount, description) VALUES ($1, $2, $3)";
 
@@ -13,17 +15,47 @@ async fn create_expense(expense: &Expense, pool: &sqlx::PgPool) -> Result<(), Bo
     Ok(())
 }
 
-async fn update_expense(expense: &Expense, pool: &sqlx::PgPool) -> Result<(), Box<dyn Error>> {
+async fn update_expense(expense: &Expense, pool: &sqlx::PgPool) -> Result<Expense, Box<dyn Error>> {
     let query = "UPDATE expenses SET amount = $1, description = $2 WHERE name = $3";
 
-    sqlx::query(query)
+    let row = sqlx::query(query)
         .bind(expense.amount)
         .bind(&expense.description)
         .bind(&expense.name)
-        .execute(pool)
+        .fetch_one(pool)
         .await?;
 
+    let expense = Expense {
+        name: row.get("name"),
+        amount: row.get("amount"),
+        description: row.get("description"),
+    };
+
+    Ok(expense)
+}
+
+async fn delete_expense(name: &str, pool: &sqlx::PgPool) -> Result<(), Box<dyn Error>> {
+    let query = "DELETE FROM expenses WHERE name = $1";
+
+    sqlx::query(query).bind(name).execute(pool).await?;
+
     Ok(())
+}
+
+async fn get_all_expenses(name: &str, pool: &sqlx::PgPool) -> Result<Vec<Expense>, Box<dyn Error>> {
+    let query = "SELECT * FROM expenses WHERE name = $1";
+
+    let rows = sqlx::query(query).bind(name).fetch_all(pool).await?;
+    let expenses = rows
+        .iter()
+        .map(|row| Expense {
+            name: row.get("name"),
+            amount: row.get("amount"),
+            description: row.get("description"),
+        })
+        .collect::<Vec<Expense>>();
+
+    Ok(expenses)
 }
 
 struct Expense {
