61489af70057da912cd788a225d6b64216cc4dc3
philippderroole <philippderroole@gmail.com>
2023-10-29 13:06:42 +0100

    enable cors

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -3,6 +3,10 @@
 mod endpoints;
 
 use serde::{Deserialize, Serialize};
+use tide::{
+    http::headers::HeaderValue,
+    security::{CorsMiddleware, Origin},
+};
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
 struct Activity {
@@ -40,6 +44,14 @@ async fn main() -> tide::Result<()> {
     sqlx::migrate!("./migrations").run(&pool).await?;
 
     let mut app = tide::with_state(pool);
+
+    let cors = CorsMiddleware::new()
+        .allow_methods("GET, POST, PUT, DELETE".parse::<HeaderValue>().unwrap())
+        .allow_origin(Origin::from("*"))
+        .allow_credentials(false);
+
+    app.with(cors);
+
     app.at("/api/activity/create")
         .post(endpoints::activity::create_activity);
     app.at("/api/activity/get")
