2b6037539169cdd21c40a42ef5cedd82ac34fb6b
philippderroole <philippderroole@gmail.com>
2023-10-29 15:17:16 +0100

    hide share on homepage

--- frontend/src/app/ShareActivity.tsx
+++ frontend/src/app/ShareActivity.tsx
@@ -1,10 +1,13 @@
 "use client";
 
 import { IconButton } from "@chakra-ui/react";
+import { useParams } from "next/navigation";
 import { BsFillShareFill } from "react-icons/bs";
 import { shareOnMobile } from "react-mobile-share";
 
 export default function ShareActivity() {
+    const params = useParams();
+
     function share() {
         shareOnMobile({
             text: "Let's share expenses!",
@@ -19,6 +22,7 @@ export default function ShareActivity() {
                 aria-label={"copy url to clipboard"}
                 colorScheme="green"
                 icon={<BsFillShareFill />}
+                hidden={params.activity_id === undefined}
                 onClick={share}></IconButton>
         </>
     );
