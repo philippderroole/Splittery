4f3f754c74d28b39766bf31dd1d0bdfea04c471b
philippderroole <philippderroole@gmail.com>
2023-10-31 13:49:30 +0100

    set api requests to use cache

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,24 +1,14 @@
-import { HttpService } from "@/services/HttpService";
 import { Center } from "@chakra-ui/react";
 import Hero from "./Hero";
 import Statistics from "./Statistics";
 
 export default async function HomePage() {
-    const activity_count = await HttpService.GET("/activity/count", "no-store");
-    const expense_count = await HttpService.GET("/expense/count", "no-store");
-    const user_count = await HttpService.GET("/user/count", "no-store");
-
     return (
         <>
             <Center flexGrow={1}>
                 <Hero />
             </Center>
-            <Statistics
-                paddingBottom="10vh"
-                activity_count={activity_count}
-                expense_count={expense_count}
-                user_count={user_count}
-            />
+            <Statistics paddingBottom="10vh" />
         </>
     );
 }
