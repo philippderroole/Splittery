9ea29de70a8342bc590f3488ce690d44180ee193
Philipp Rolli <philipp.rolli@t-online.de>
2024-02-23 23:36:22 +0100

    adjusted hero

--- frontend/src/app/components/Hero.tsx
+++ frontend/src/app/components/Hero.tsx
@@ -1,27 +1,20 @@
 "use client";
 
 import { HttpService } from "@/services/HttpService";
-import {
-    Button,
-    Flex,
-    HStack,
-    Hide,
-    Show,
-    Stack,
-    Text,
-} from "@chakra-ui/react";
+import { Button, Stack, Text } from "@chakra-ui/react";
 import { useRouter } from "next/navigation";
 
 export default function Hero() {
     const router = useRouter();
 
-    const button_width = ["2xs", "3xs"];
-
     const heading = (
-        <Text fontWeight="600" fontSize={["3xl", "4xl", "5xl", "6xl"]}>
+        <Text
+            fontWeight="1000"
+            fontSize={["3xl", "4xl", "5xl", null, "6xl", "7xl"]}>
             <Text
                 as="span"
-                bgGradient="linear(to-l, #4F6F52, #9AE6B4)"
+                backgroundColor={"green.500"}
+                //bgGradient="linear(to-l, #4F6F52, #9AE6B4)"
                 bgClip="text">
                 Split{" "}
             </Text>
@@ -31,66 +24,60 @@ export default function Hero() {
     );
 
     const subHeading = (
-        <Text fontSize={["lg", null, "2xl", "3xl"]} opacity="0.7">
+        <Text fontSize={["lg", "xl", "2xl", null, "3xl", "4xl"]} opacity="0.7">
             Easily track shared expenses, split bills, and settle up together.
         </Text>
     );
 
-    const createSplitButton = (
-        <Button
-            paddingX={5}
-            paddingY={[6, 8]}
-            colorScheme="green"
-            variant="solid"
-            size={["md", "lg"]}
-            width={button_width}
-            onClick={() => {
-                HttpService.POST("/splits", {
-                    name: "My new split",
-                }).then((split) => {
-                    router.push("/" + split.id);
-                });
-            }}>
-            Create a split →
-        </Button>
-    );
+    const buttonGroup = () => {
+        const paddingX = { lg: 5, xl: 6 };
+        const paddingY = { lg: 6, xl: 8 };
+        const fontSize = ["md", "lg", null, null, "xl"];
+        const width = { base: 200, sm: 250, md: 200 };
+        const height = { base: 50, sm: 50, md: 61, lg: 70 };
 
-    const browseSplitsButton = (
-        <Button
-            paddingX={5}
-            paddingY={[6, 8]}
-            colorScheme="white"
-            variant="outline"
-            size={["md", "lg"]}
-            width={button_width}>
-            Browse my splits
-        </Button>
-    );
-
-    const buttonGroup = (
-        <>
-            <Show above="sm">
-                <HStack marginTop={8}>
-                    {createSplitButton}
-                    {browseSplitsButton}
-                </HStack>
-            </Show>
-            <Hide above="sm">
-                <Flex direction="column" align="center">
-                    <Stack spacing={5} marginTop={8}>
-                        {createSplitButton}
-                        {browseSplitsButton}
-                    </Stack>
-                </Flex>
-            </Hide>
-        </>
-    );
+        return (
+            <Stack
+                direction={{ base: "column", md: "row" }}
+                alignItems={{ base: "center", md: "start" }}
+                spacing={5}
+                marginTop={8}>
+                <Button
+                    paddingX={paddingX}
+                    paddingY={paddingY}
+                    fontSize={fontSize}
+                    width={width}
+                    height={height}
+                    colorScheme="green"
+                    variant="solid"
+                    onClick={() => {
+                        HttpService.POST("/splits", {
+                            name: "My new split",
+                        }).then((split) => {
+                            router.push("/" + split.id);
+                        });
+                    }}>
+                    Create a split →
+                </Button>
+                <Button
+                    paddingX={paddingX}
+                    paddingY={paddingY}
+                    fontSize={fontSize}
+                    width={width}
+                    height={height}
+                    colorScheme="white"
+                    variant="outline">
+                    Browse my splits
+                </Button>
+            </Stack>
+        );
+    };
 
     return (
         <Stack padding={10}>
             {heading}
             {subHeading}
-            {buttonGroup}
+            {buttonGroup()}
         </Stack>
     );
 }
