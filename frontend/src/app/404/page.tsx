50a1b6bf38cf0847e18415722f2589c26e151cdf
Philipp Rolli <philipp.rolli@t-online.de>
2024-02-29 00:48:07 +0100

    implemented not found page

---
+++ frontend/src/app/404/page.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import { Button, Center, Flex, Heading, Stack } from "@chakra-ui/react";
+import { useRouter } from "next/navigation";
+
+export default function NotFoundPage() {
+    const router = useRouter();
+
+    return (
+        <Center flexGrow={1} paddingBottom={20}>
+            <Stack>
+                <Heading padding={8} fontSize={60}>
+                    404 - Page Not Found
+                </Heading>
+                <Flex direction="row" justifyContent="center">
+                    <Button
+                        colorScheme="primary"
+                        width="fit-content"
+                        onClick={() => {
+                            router.push("/");
+                        }}>
+                        Go Home
+                    </Button>
+                </Flex>
+            </Stack>
+        </Center>
+    );
+}
