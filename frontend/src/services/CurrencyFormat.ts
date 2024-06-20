2f85ec056ba92f3150dfd5533dc2f44403f6e1e2
Philipp Rolli <philipp.rolli@t-online.de>
2024-06-20 10:33:25 +0200

    round currency two to decimals

--- frontend/src/services/CurrencyFormat.ts
+++ frontend/src/services/CurrencyFormat.ts
@@ -2,4 +2,10 @@ export abstract class CurrencyFormat {
     static format(amount: number): string {
         return amount.toFixed(2) + "€";
     }
+
+    static round(amount: number, decimals: number): number {
+        return (
+            Math.round(amount * Math.pow(10, decimals)) / Math.pow(10, decimals)
+        );
+    }
 }
